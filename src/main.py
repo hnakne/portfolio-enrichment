c227cd77bf1730dccfb0512b774155e85c6fe39e
Henrik Näkne <henrik_nakne@discovery.com>
2022-11-24 20:03:31 +0100

    Save data structured

--- src/main.py
+++ src/main.py
@@ -63,21 +63,21 @@ def check_and_print_metrics(items: list, url: str):
     print(f'# of parsed lines: {len(valid_elems)}')
 
 
-def save_to_json(dir: str, output_file_name: str, l: list):
+def save_to_json(dir: str, output_file_name: str, items: list):
     Path(dir).mkdir(parents=True, exist_ok=True)
-    with open(output_file_name, 'w') as f:
-        for d in l:
+    with open(f'{dir}/{output_file_name}', 'w') as f:
+        for d in items:
             json.dump(d, f)
             f.write('\n')
     f.close()
 
 
-def build_dir_path(date, data_name):
+def build_dir_path(data_name, date: datetime.date):
     return f'output/{data_name}/{date}'
 
 
-def today_as_string():
-    return datetime.date.today().isoformat()
+def to_string(dt: datetime.date):
+    return dt.isoformat()
 
 
 # basic sanity checks
@@ -98,10 +98,10 @@ def items_from_page_data(j: dict):
     return items
 
 
-def enrich(items: list, url: str, date: str):
+def enrich(items: list, url: str, date_str: str):
     for item in items:
         item['source_url'] = url
-        item['date'] = date
+        item['date'] = date_str
     # don't have to return but easier to reason about
     return items
 
@@ -114,27 +114,29 @@ def curl_and_get(url: str) -> dict:
         raise Exception(f'Not 200: {str(r)}')
 
 
-def process(page_data_response: dict, url: str, date: str) -> (dict, list):
+def process(page_data_response: dict, url: str, date: datetime.date) -> (dict, list):
     items = items_from_page_data(page_data_response)
-    enriched_portfolio_items = enrich(items=items, url=url, date=date)
+    enriched_portfolio_items = enrich(items=items, url=url, date_str=to_string(date))
     return enriched_portfolio_items
 
 
+def save(data_source_name: str, items: list, date: datetime.date):
+    directory = build_dir_path(data_source_name, date)
+    save_to_json(dir=directory, output_file_name='output.json', items=items)
+
+
 def main_curl():
-    today_str = today_as_string()
+    today = datetime.date.today()
     # separate from divestments
     portfolio_data = curl_and_get(portfolio_page_data_url)
-    enriched_portfolio_items = process(portfolio_data, portfolio_page_data_url, str)
+    enriched_portfolio_items = process(portfolio_data, portfolio_page_data_url, date=today)
     print_basic_metrics(enriched_portfolio_items, portfolio_page_data_url)
-    save_to_json(f'output/output_portfolio_{today_str}.json', enriched_portfolio_items)
+    save('portfolio', enriched_portfolio_items, today)
 
     divestments_data = curl_and_get(divestments_page_data_url)
-    enriched_divestment_items = process(divestments_data, divestments_page_data_url, date=today_str)
+    enriched_divestment_items = process(divestments_data, divestments_page_data_url, date=today)
     print_basic_metrics(enriched_divestment_items, divestments_page_data_url)
-    save_to_json(f'output/output_divestments_{today_str}.json', enriched_portfolio_items)
-
-    output = enriched_divestment_items + enriched_portfolio_items
-    save_to_json()
+    save('divestments', enriched_divestment_items, today)
 
 
 def main(args: list):
